061a70216f2fc4ce99aebec09f7ae4c0ed8cc2c1
Shlok Patel <51414879+madrix01@users.noreply.github.com>
2021-01-16 21:04:03 +0530

    Added switch color change

--- src/Pages/devices.js
+++ src/Pages/devices.js
@@ -11,21 +11,22 @@ class DevicesInRoom extends React.Component{
       devices : [],
     }
     this.getDevices = this.getDevices.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   async getDevices(){
     let {id} = this.props.match.params;
     const res = await fetch(`http://192.168.29.69:8000/devices/room/${id}`);
     const data = await res.json();
-    this.setState({devices : data});
+    console.log('executed');
     return data;
   }
 
-  async handleSubmit(dev){
+  handleSubmit(dev, i){
     return event => {
       const sendIoReq = async () => { 
         var reqBody = `boardId=${dev.boardId}&pinAdress=${dev.pinAdress}&stateTc=${!dev.state}`;
-        dev.state = !dev.state
+        //dev.state = !dev.state
         console.log(reqBody);
         const response = await fetch('http://192.168.29.69:8000/io/', {
           method: 'POST',
@@ -37,7 +38,10 @@ class DevicesInRoom extends React.Component{
         const data = await response.json();
         console.log(data);
       }
-      sendIoReq().then(console.log("Request sent")).then(console.log(this.state.devices[1]));
+      let dvcs = this.state.devices;
+      dvcs[i].state = !dev.state;
+      this.setState({devices : dvcs});
+      sendIoReq();
       event.preventDefault();
     }
   }
@@ -46,37 +50,36 @@ class DevicesInRoom extends React.Component{
   async componentDidMount(){
     const devices = await this.getDevices();
     this.setState({devices : devices});
+    console.log(this.state.devices);
   }
 
-  // this.state.devices[index].state?"green": "red"
   render(){
-    const Device = (devc, index) => (
-      <div>
-        <Card elevation={5} className="root1" variant="outlined" style={{background: 'black' , borderColor:'#424242'}} onClick={this.handleSubmit(devc.devc)}>
-          <CardActionArea style={{height : "100%", color: '#424242'}}>
-            <div className="root2">
-              <Typography variant="h4">{devc.devc.deviceName}</Typography>
-              <Typography variant="h4">{devc.devc.state.toString()}</Typography>
-            </div>
-          </CardActionArea>
-        </Card>
-      </div>
+    console.log('length' ,this.state.devices.length);
+    if(!this.state.devices.length){
+      console.log('its null');
+      return null;
+    }
+    let Device = () => (
+      <div className='rms'>
+        {this.state.devices.map((devc, i) => (
+          <div>
+            <Card elevation={5} className="root1" variant="outlined" style={{background:  this.state.devices[i].state?"green": "red" , borderColor:'#424242'}} onClick={this.handleSubmit(devc,i)}>
+              <CardActionArea style={{height : "100%", color: '#424242'}}>
+              <div className="root2">
+                <Typography variant="h4">{devc.deviceName}</Typography>
+                <Typography variant="h4">{devc.state.toString()}</Typography>
+              </div>
+              </CardActionArea>
+            </Card>
+          </div>
+        ))}
+      </div>      
     )
 
     return(
       <div>
         <AppBarStyled name="Devices"/>
-        <div className="rms">
-        {this.state.devices.map((rm, index) => (
-          <div>
-          <Device
-            devc={rm}
-            index={index}
-          />
-          </div>
-        ))
-        }
-        </div>
+        <Device/>
       </div>
     )
   }
